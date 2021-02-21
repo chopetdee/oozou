5ef97a9a8058fe66f9863c602d14c5ca91d6d6c6
Cho <cho.petdee@gmail.com>
2021-02-21 23:16:51 +0700

    first

--- db/index.js
+++ db/index.js
@@ -12,21 +12,35 @@ const {
 // const Blogpost = require('./models/blogpost')
 
 
-const db = new Sequelize('postgres://localhost:5432/template_db', {
-  logging: false
-}) // Example for postgres
+
+const db = new Sequelize('postgres://root:root@localhost:5432/mydb')
+// const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname') 
+
+// const db = new Sequelize(`postgres`, `cho`, ``, {
+//   host: "postgres://postgres:postgres@localhost/postgres",
+//   dialect: 'postgres'
+// });
+
+// const db = new Sequelize("postgres://postgres:postgres@localhost/postgres", {
+//   dialect: 'postgres'
+//   // anything else you want to pass
+// })
+// const db = new Sequelize('postgres://postgres:postgres@localhost/postgres', {
+//   // const db = new Sequelize('postgres://localhost:5432/postgres', 'cho', '', {
+//   logging: false
+// }) // Example for postgres
 
 const User = db.define("user", {
   name: DataTypes.TEXT,
-  
+
 });
 
 const BlogPost = db.define("blogpost", {
   title: DataTypes.TEXT,
-  content:DataTypes.TEXT,
+  content: DataTypes.TEXT,
   dataDate: {
-      type: DataTypes.DATE, 
-    },
+    type: DataTypes.DATE,
+  },
 });
 
-module.exports =  {db, User, BlogPost}
+module.exports = { db, User, BlogPost }
